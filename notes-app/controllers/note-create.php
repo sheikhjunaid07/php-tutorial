7dbc6ad6d88acd0472d055666c65a7c8b5e1eb4b
sheikhjunaid07 <sheikhjunaid732000@gmail.com>
2026-04-21 18:40:36 +0530

    Refactor note creation form to include body input and handle POST requests for note submission

--- notes-app/controllers/note-create.php
+++ notes-app/controllers/note-create.php
@@ -1,5 +1,16 @@
 <?php
 
+$config = require('config.php');
+$db = new Database($config['database']);
+
 $heading = "Create New Note";
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $db->query("INSERT INTO notes VALUES (:body, :user_id)", [
+        "body" => $POST['body'],
+        "user_id" => 1,
+
+    ]);
+}
 
 require "views/note-create.view.php";
