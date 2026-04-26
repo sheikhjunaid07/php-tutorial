99cc3fa9ce26d3a12ad475af929f6e04fb66c625
sheikhjunaid07 <sheikhjunaid732000@gmail.com>
2026-04-26 13:19:34 +0530

    Add validation for note body input and display error messages

--- notes-app/views/note-create.view.php
+++ notes-app/views/note-create.view.php
@@ -13,6 +13,11 @@
                         <div class="mt-2">
                             <textarea id="body" name="body" rows="5" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"></textarea>
                         </div>
+
+                        <!-- print errors -->
+                        <?php if (isset($errors['body'])) : ?>
+                            <p class="text-red-500 text-s mt-2"><?= $errors['body'] ?></p>
+                        <?php endif; ?>
                     </div>
 
                 </div>
